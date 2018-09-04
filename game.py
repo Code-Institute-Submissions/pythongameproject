4afdeeacf34fd4ea21de0fba368a68e4af988d59
Micaela Signorelli <m.signorelli@gmail.com>
2018-09-04 19:30:16 +0000

    begins code for game

---
+++ game.py
@@ -0,0 +1,12 @@
+guess = 0
+
+while True:
+    print("Which is the animal that walks with four legs in the morning, two in the afternoon and three at night")
+    print("Enter your guess! For a hint type 'hint', or 'I give up', below")
+    answer = input()
+    guess += 1
+
+    if answer == human
+        print("You win!")
+        print("It took you " + str(guess) + " guesses.")
+        break
