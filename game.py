4fb4d8f55c574149734ff9eb78179302c99ea97c
Micaela Signorelli <m.signorelli@gmail.com>
2018-09-08 11:42:04 +0000

    adds line breaks and formatting

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ guess = 0
 
 while True:
     print("Enter your guess below! For a hint type 'hint', or, if you give up, type: 'I give up'")
-    print("Which is the animal that walks with four legs in the morning, two in the afternoon and three at night")
+    print("Your first question is: \n 'What creature walks on four legs in the morning, two legs in the afternoon, and three legs in the evening'")
     answer = input()
     guess += 1
 
@@ -21,7 +21,10 @@ while True:
         print("You are a...")
 
     elif answer.upper() == "I GIVE UP":
-        print("The answer was human!")
+        print("The answer was 'human'!")
 
     else:
         print("Sorry, keep trying")
+        
+        
+# There are 2 sisters: one gives birth to the other and she, in turn, gives birth to the first. Who are the two sisters? Day and Night
