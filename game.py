27f7d138ddcc09f980bd07f801833bee7d6d6357
Micaela Signorelli <m.signorelli@gmail.com>
2018-09-08 15:14:33 +0000

    Improves coding of game.py with video lesson info

--- game.py
+++ game.py
@@ -1,30 +1,49 @@
 # from byotest import *
 
 # print("All tests pass")
-
-print("Welcome to the Riddle game!")
-
-guess = 0
-
-while True:
-    print("Enter your guess below! For a hint type 'hint', or, if you give up, type: 'I give up'")
-    print("Your first question is: \n 'What creature walks on four legs in the morning, two legs in the afternoon, and three legs in the evening'")
-    answer = input()
-    guess += 1
-
-    if "human" in answer:
-        print("You win!")
-        print("It took you " + str(guess) + " guesses.")
-        sys.exit()
+def show_menu():
+    print("1. Play the Riddle Game!")
+    print("2. Add your Riddle")
+    print("3. Quit")
     
-    elif answer == "hint":
-        print("You are a...")
+    option = input("Enter option number: ")
+    return option
 
-    elif answer.upper() == "I GIVE UP":
-        print("The answer was 'human'!")
 
-    else:
-        print("Sorry, keep trying")
+#print("Welcome to the Riddle game!")
+#guess = 0
+
+def game_loop():   
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("You have selected 'Play the Riddle Game!'")
+        elif option == "2":
+            print("You have selected 'Add your Riddle'")
+        elif option == "3":
+            break
+        else:
+            print("Invalid Option :( ")
+game_loop()
+       
+#        print("Enter your guess below! For a hint type 'hint', or, if you give up, type: 'I give up'")
+#        print("Your first question is: \n 'What creature walks on four legs in the morning, two legs in the afternoon, and three legs in the evening'")
+#        answer = input()
+#        guess += 1
+#
+#        if "human" in answer:
+#            print("You win!")
+#            print("It took you " + str(guess) + " guesses.")
+#            sys.exit()
+    
+#        elif answer == "hint":
+#            print("You are a...")
+
+#        elif answer.upper() == "I GIVE UP":
+#            print("The answer was 'human'!")
+
+#        else:
+#            print("Sorry, keep trying")
         
         
 # There are 2 sisters: one gives birth to the other and she, in turn, gives birth to the first. Who are the two sisters? Day and Night
