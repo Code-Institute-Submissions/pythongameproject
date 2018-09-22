20fbb6384a1a7c8b045cb358751c8efa79184733
Micaela Signorelli <m.signorelli@gmail.com>
2018-09-22 23:22:31 +0000

    WIP tidied test implementation

--- features/steps/game.py
+++ features/steps/game.py
@@ -7,27 +7,18 @@ def step_impl(context):
     context.riddle = Riddle()
     context.correct = None
 
-@given(u'the riddle "what is the colour" is asked')
-def step_impl(context):
-    context.riddle.question = "what is the colour"
+@given(u'the riddle "{question}" is asked')
+def step_impl(context, question):
+    context.riddle.question = question
 
-@given(u'the correct answer is "yellow"')
-def step_impl(context):
-    context.riddle.answer = "yellow"
+@given(u'the correct answer is "{answer}"')
+def step_impl(context, answer):
+    context.riddle.answer = answer
 
-@when(u'we give the answer "yellow"')
-def step_impl(context):
-    context.correct = context.riddle.checkAnswer("yellow")
+@when(u'we give the answer "{answer}"')
+def step_impl(context, answer):
+    context.correct = context.riddle.checkAnswer(answer)
 
-@then(u'answer is correct')
-def step_impl(context):
-    assert context.correct is True
-
-@when(u'we give the answer "red"')
-def step_impl(context):
-    context.correct = context.riddle.checkAnswer("red")
-
-
-@then(u'answer is incorrect')
-def step_impl(context):
-    assert context.correct is False
+@then(u'answer is {correct}')
+def step_impl(context, correct):
+    assert context.correct is (correct == "correct")
