d1b2f9f31a22be25a8849d559c12a5fe0bbb87fb
Micaela Signorelli <m.signorelli@gmail.com>
2018-10-27 15:04:05 +0000

    implements second chance to answer riddle

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import sqlite3
 import os
 from flask import Flask, render_template, request, redirect, url_for, session
-from Riddle import Riddle
-from Score import Score
-from User import User
+from riddle import Riddle
+from score import Score
+from user import User
 
 # import 'RiddleRepository.py'
 
@@ -60,16 +60,21 @@ def showRiddle():
     riddle = Riddle.get_unanswered_riddle_for_user(user)
     if riddle is None:  
         return render_template('riddlesover.html')    
-    return render_template('ask_riddle.html', riddle=riddle)
+    return render_template('ask_riddle.html', riddle=riddle, firstAttempt=True)
 
 @app.route('/riddle/<int:riddle_id>', methods=['POST'])
 def answerRiddle(riddle_id):
     riddle = Riddle(riddle_id)
+    firstAttempt = request.form['first_attempt']
+    firstAttempt = True if request.form['first_attempt'] == "True" else False
 
 # decide if answer is correct
 # render button going to the next riddle (if there are any)
 
     riddle_correct = riddle.checkAnswer(request.form['answer'])
+    if riddle_correct is False and firstAttempt is True:
+        return render_template('ask_riddle.html', riddle=riddle, firstAttempt=False)
+        
     Score.record(session.get('logged_in_user_id'),riddle.id,riddle_correct)
     
     return render_template('answer_riddle.html', correct=riddle_correct, riddle=riddle)
